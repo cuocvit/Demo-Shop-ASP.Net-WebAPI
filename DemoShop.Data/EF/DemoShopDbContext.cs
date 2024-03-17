0cac61ea1ad3bf90f510b997f839857602639fcd
Quoc Viet <nguyenquocviet1203.thanhthuy@gmail.com>
2024-03-17 22:59:44 +0700

    Add Entity

---
+++ DemoShop.Data/EF/DemoShopDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DemoShop.Data.Entities;
+
+namespace DemoShop.Data.EF
+{
+    public class DemoShopDbContext : DbContext
+    {
+        public DemoShopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+    }
+}
